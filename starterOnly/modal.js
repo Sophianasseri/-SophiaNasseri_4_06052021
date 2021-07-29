a7ddf3d1e1d6168c223b2eac5ac01a4304645c64
Sophia Nasseri <nasseri.sophia@gmail.com>
2021-07-29 12:09:58 +0200

    Ajout validation prénom

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,13 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeModal = document.querySelector(".close");
+const form = document.getElementById("suscribe");
+
+// Form fields
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const quantity = document.getElementById("quantity");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -27,7 +34,67 @@ closeModal.addEventListener("click", () => {
   modalbg.style.display = "none";
 });
 
-closeModal.addEventListener("click", () => {
-  answer.classList.add("show-answer");
-  answer.style.color = "red";
+// Form submit validation
+
+const isRequired = (value) => {
+  if (value === "") {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const minLength = (length) => {
+  if (length < 2) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+// Email Regex
+
+const isEmailValid = (email) => {
+  if (
+    !value.match(
+      /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    )
+  ) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const textError = (message) => {
+  const error = document.querySelector(".error");
+  console.log(error);
+  error.textContent = message;
+};
+
+const checkFirstName = () => {
+  let valid = false;
+
+  const name = firstName.value;
+
+  if (!isRequired(name)) {
+    textError("Veuillez entrer un prénom");
+  } else if (!minLength(name.length)) {
+    textError("Veuillez entrer 2 caractères minimum ");
+  } else {
+    valid = true;
+  }
+  return valid;
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // if (firstName.value.length < 2) {
+  //   textError.innerHTML = "<p>Veuillez entrer au moins 2 caractères</p>";
+  // } else if (lastName.value.length < 2) {
+  //   textError.innerHTML = "<p>Veuillez entrer au moins 2 caractères</p>";
+  // }
 });
+function validate() {
+  checkFirstName();
+}
