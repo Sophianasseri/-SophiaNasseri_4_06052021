3af2895778440e9d6a45f457473ee93449a2fdda
Sophia Nasseri <nasseri.sophia@gmail.com>
2021-08-05 17:16:01 +0200

    Validation du formulaire si toutes les conditions sont remplies

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -45,99 +45,99 @@ closeModal.addEventListener("click", () => {
 
 // Form submit validation
 
-const isRequired = (value) => {
-  if (value === "") {
-    return false;
-  } else {
-    return true;
-  }
-};
+const validate = (event) => {
+  const isRequired = (value) => {
+    if (value === "") {
+      return false;
+    } else {
+      return true;
+    }
+  };
 
-const minLength = (length) => {
-  if (length < 2) {
-    return false;
-  } else {
-    return true;
-  }
-};
-const border = () => {
+  const minLength = (length) => {
+    if (length < 2) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  // Email Regex
+  const isEmailValid = (email) => {
+    const regex =
+      /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    return regex.test(email);
+  };
+
+  const textError = (tag, message) => {
+    const span = document.querySelector("." + tag + "-container .error");
+    span.textContent = message;
+  };
+
   const inputs = Array.from(inputTextType);
-  inputs.map((input) => {
-    if (input.value === "") {
+  inputs.map((input, index) => {
+    console.log(index);
+    if (input.value === "" || input.value.length < 2 || !isEmailValid()) {
       input.classList.add("input-border");
     } else {
       input.classList.remove("input-border");
+      return true;
     }
   });
-};
 
-// inputs.forEach((input) => input.classList.add("input-border"));
+  //First name validation
 
-// Email Regex
-const isEmailValid = (email) => {
-  const regex =
-    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  return regex.test(email);
-};
-
-const textError = (tag, message) => {
-  const span = document.querySelector("." + tag + "-container .error");
-  span.textContent = message;
-};
-
-//First name validation
-const checkFirstName = () => {
   const firstNameField = firstName.value;
 
   if (!isRequired(firstNameField)) {
     textError("firstname", "Veuillez entrer un prénom");
+    event.preventDefault();
   } else if (!minLength(firstNameField.length)) {
     textError("firstname", "Veuillez entrer 2 caractères minimum ");
+    event.preventDefault();
   } else {
     textError("firstname", "");
   }
-};
 
-const checkLastName = () => {
   const lastNameField = lastName.value;
 
   if (!isRequired(lastNameField)) {
     textError("lastname", "Veuillez entrer un nom");
+    event.preventDefault();
   } else if (!minLength(lastNameField.length)) {
     textError("lastname", "Veuillez entrer 2 caractères minimum ");
+    event.preventDefault();
   } else {
     textError("lastname", "");
   }
-};
 
-const checkEmail = () => {
   const emailField = email.value;
   if (!isRequired(emailField)) {
     textError("email", "Veuillez entrer un e-mail");
+    event.preventDefault();
   } else if (!isEmailValid(emailField)) {
     textError("email", "Veuillez entrer un email valide");
+    event.preventDefault();
   } else {
     textError("email", "");
   }
-};
 
-const checkBirthDate = () => {
   const dateField = birthDate.value;
   if (!isRequired(dateField)) {
     textError("birthdate", "Veuillez entrer une date de naissance");
+    event.preventDefault();
   } else {
     textError("birthdate", "");
   }
-};
-const checkQuantity = () => {
+
   const quantityField = quantity.value;
   if (!isRequired(quantityField)) {
     textError("quantity", "Veuillez entrer un nombre");
+    event.preventDefault();
   } else {
     textError("quantity", "");
   }
-};
-const checkRadio = () => {
+
   if (
     location1.checked == false &&
     location2.checked == false &&
@@ -147,33 +147,14 @@ const checkRadio = () => {
     location6.checked == false
   ) {
     textError("radio", "Veuillez sélectionner une ville");
+    event.preventDefault();
   } else {
     textError("radio", "");
   }
-};
-const checkCheckbox = () => {
   if (!checkbox.checked) {
     textError("checkbox", "Veuillez accepter  les conditions d'utilisation");
+    event.preventDefault();
   } else {
     textError("checkbox", "");
   }
 };
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  // if (firstName.value.length < 2) {
-  //   textError.innerHTML = "<p>Veuillez entrer au moins 2 caractères</p>";
-  // } else if (lastName.value.length < 2) {
-  //   textError.innerHTML = "<p>Veuillez entrer au moins 2 caractères</p>";
-  // }
-});
-function validate() {
-  checkFirstName();
-  checkLastName();
-  checkEmail();
-  checkBirthDate();
-  checkQuantity();
-  checkRadio();
-  checkCheckbox();
-  border();
-}
